fab6d83327ead8e77a0a7241e8a5461b177c1a56
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-14 13:39:24 +0000

    add intent and get first version of App working on phone

--- app/src/main/java/com/codeclan/eightball/AnswerActivity.java
+++ app/src/main/java/com/codeclan/eightball/AnswerActivity.java
@@ -1,13 +1,29 @@
 package com.codeclan.eightball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AnswerActivity extends AppCompatActivity {
+
+    TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();                   //retrieve parameters from intent
+        String question = extras.getString("question");       // get object via key
+
+
+        answerText = (TextView)findViewById(R.id.answer_text);
+//        answerText.setText(question);                       // development step
+
+        Answers answers = new Answers();                       // completing the job
+        String answer = answers.getAnswer();                   // using Answers class put together yesterday
+        answerText.setText(answer);
     }
 }
